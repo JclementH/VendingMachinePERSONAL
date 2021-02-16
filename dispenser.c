402a821151fef3b1ea4f0e87c83e02ec7bf07261
JclementH <78693828+JclementH@users.noreply.github.com>
2021-02-16 09:15:38 +0800

    uv.0.1
    Update version 0.1:
    * added fclose to close fopen
    * added notes for clarification

--- dispenser.c
+++ dispenser.c
@@ -3,16 +3,18 @@
 #include "cashregister.h"
 
 int getCount(int selected, int quantity) {
+	//read inventory.txt
 	invChange = fopen("inventory.txt", "w");
+	//cases from selected
 	switch(selected) {
 		case 1 :	if(inv[0].numberOfItems != 0 && inv[0].numberOfItems >= quantity) {
 						inv[0].numberOfItems = inv[0].numberOfItems - quantity;
-						//
+						//rewrite file ; change inventory 
 						fprintf(invChange, "\n%i\n", inv[0].numberOfItems);
 						fprintf(invChange, "%i\n", inv[1].numberOfItems);
 						fprintf(invChange, "%i\n", inv[2].numberOfItems);
 						fprintf(invChange, "%i\n", inv[3].numberOfItems);
-						//
+						//proceed to getProductCost()
 						getProductCost(inv[0].cost, quantity);
 					} else if(inv[0].numberOfItems != 0 && inv[0].numberOfItems < quantity) {
 						printf("Sorry! The dispenser does not have enough of this item (%s)\nto meet the quantity you have inputted. Please try again.\n", inv[0].nameOfItem);
@@ -24,12 +26,12 @@ int getCount(int selected, int quantity) {
 					break;
 		case 2 :	if(inv[1].numberOfItems != 0 && inv[1].numberOfItems >= quantity) {
 						inv[1].numberOfItems = inv[1].numberOfItems - quantity;
-						//
+						//rewrite file ; change inventory 
 						fprintf(invChange, "\n%i\n", inv[0].numberOfItems);
 						fprintf(invChange, "%i\n", inv[1].numberOfItems);
 						fprintf(invChange, "%i\n", inv[2].numberOfItems);
 						fprintf(invChange, "%i\n", inv[3].numberOfItems);
-						//
+						//proceed to getProductCost()
 						getProductCost(inv[1].cost, quantity);
 					} else if(inv[1].numberOfItems != 0 && inv[1].numberOfItems < quantity) {
 						printf("Sorry! The dispenser does not have enough of this item (%s)\nto meet the quantity you have inputted. Please try again.\n", inv[1].nameOfItem);
@@ -41,13 +43,13 @@ int getCount(int selected, int quantity) {
 					break;
 		case 3 :	if(inv[2].numberOfItems != 0 && inv[2].numberOfItems >= quantity) {
 						inv[2].numberOfItems = inv[2].numberOfItems - quantity;
-						//
+						//rewrite file ; change inventory
 						fprintf(invChange, "\n%i\n", inv[0].numberOfItems);
 						fprintf(invChange, "%i\n", inv[1].numberOfItems);
 						fprintf(invChange, "%i\n", inv[2].numberOfItems);
 						fprintf(invChange, "%i\n", inv[3].numberOfItems);
 						fprintf(invChange, "%i\n", inv[2].numberOfItems);
-						//
+						//proceed to getProductCost()
 						getProductCost(inv[2].cost, quantity);
 					} else if(inv[2].numberOfItems != 0 && inv[2].numberOfItems < quantity) {
 						printf("Sorry! The dispenser does not have enough of this item (%s)\nto meet the quantity you have inputted. Please try again.\n", inv[2].nameOfItem);
@@ -59,13 +61,13 @@ int getCount(int selected, int quantity) {
 					break;
 		case 4 :	if(inv[3].numberOfItems != 0 && inv[3].numberOfItems >= quantity) {
 						inv[3].numberOfItems = inv[3].numberOfItems - quantity;
-						//
+						//rewrite file ; change inventory
 						fprintf(invChange, "\n%i\n", inv[0].numberOfItems);
 						fprintf(invChange, "%i\n", inv[1].numberOfItems);
 						fprintf(invChange, "%i\n", inv[2].numberOfItems);
 						fprintf(invChange, "%i\n", inv[3].numberOfItems);
 						fprintf(invChange, "%i\n", inv[3].numberOfItems);
-						//
+						//proceed to getProductCost()
 						getProductCost(inv[3].cost, quantity);
 					} else if(inv[3].numberOfItems != 0 && inv[3].numberOfItems < quantity) {
 						printf("Sorry! The dispenser does not have enough of this item (%s)\nto meet the quantity you have inputted. Please try again.\n", inv[3].nameOfItem);
@@ -76,19 +78,24 @@ int getCount(int selected, int quantity) {
 					}
 					break;
 	}
+	//close inventory.txt
+	fclose(invChange);
+	//return 0 to exit
 	return 0;
 }
 
 int getProductCost (int cost, int quantity) {
 	int totalCost;
-	//
+	//Get total cost by multiplying cost with quantity
 	totalCost = cost*quantity;
 	makeSale(totalCost);
-	//
+	//return 0 to exit
 	return 0;
 }
 
 int makeSale(int totalCost) {
+	//proceeds to currentBalance()
 	currentBalance(0, totalCost);
+	//return 0 to exit
 	return 0;
 }
